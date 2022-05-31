7cb8d37141e505426222370f1c2a401a25c97dac
mathed1as <matheus.vex@gmail.com>
2022-05-31 17:34:24 -0300

    add lint staged

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts'
+  ],
+  coverageDirectory: "coverage",
+  moduleNameMapper: {
+    '@/tests(.+)': '<rootDir>/tests/$1',
+    '@/(.+)': '<rootDir>/src/$1'
+  },
+  roots: [
+    '<rootDir>/src',
+    '<rootDir>/tests'
+  ],
+  transform: {
+    '\\.ts$': 'ts-jest'
+  }
+};
