4e8d0fd0cc25579849fe07cfaf7f535f462a7df8
mathed1as <matheus.vex@gmail.com>
2022-06-15 19:13:49 -0300

    add controller error

--- src/application/controllers/process-transaction-controller.ts
+++ src/application/controllers/process-transaction-controller.ts
@@ -1,10 +1,22 @@
 import { ProcessTransactionService } from '@/data/services'
+import { HttpResponse } from '@/application/helpers/http'
+import { TransactionDto } from '@/domain/models/transaction-dto'
+
+type HttpRequest = {
+  body: TransactionDto
+  token: string
+}
 
 export class ProcessTransactionController {
   constructor (private readonly ProcessTransactionService: ProcessTransactionService) {}
-  async handle (httpRequest: any): Promise<any> {
-    console.log({ httpRequest })
-    const result = await this.ProcessTransactionService.exec(httpRequest)
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    if (httpRequest.token === undefined) {
+      return {
+        statusCode: 400,
+        data: new Error('Authentication token is required')
+      }
+    }
+    const result = await this.ProcessTransactionService.exec(httpRequest.body)
 
     return {
       statusCode: 200,
