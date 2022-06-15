871836be38470400e489c24a96a1dddf32bb1444
mathed1as <matheus.vex@gmail.com>
2022-06-15 17:40:35 -0300

    adpt express route to suporte token

--- src/application/controllers/process-transaction-controller.ts
+++ src/application/controllers/process-transaction-controller.ts
@@ -3,6 +3,7 @@ import { ProcessTransactionService } from '@/data/services'
 export class ProcessTransactionController {
   constructor (private readonly ProcessTransactionService: ProcessTransactionService) {}
   async handle (httpRequest: any): Promise<any> {
+    console.log({ httpRequest })
     const result = await this.ProcessTransactionService.exec(httpRequest)
 
     return {
