0919c1ccfef95dcae3eab52382a6b5a6ca721df6
mathed1as <matheus.vex@gmail.com>
2022-06-16 14:28:54 -0300

    refactor repository logic

--- src/infra/typeorm/repos/process-transaction-repo.ts
+++ src/infra/typeorm/repos/process-transaction-repo.ts
@@ -5,21 +5,17 @@ import { PgPayable, PgTransaction } from '@/infra/typeorm/entities'
 export class ProcessTransactionRepoository implements ProcessTransactionRepo {
   constructor (private readonly dataSource: DataSource) {}
   async savePayable (params: ProcessTransactionRepo.PayableParams): Promise<ProcessTransactionRepo.PayableResult> {
-    try {
-      const { value, status, paymentDateHour } = params
-      const insertResult = await this.dataSource
-        .createQueryBuilder()
-        .insert()
-        .into(PgPayable)
-        .values([
-          { value, status, paymentDateHour }
-        ])
-        .execute()
+    const { value, status, paymentDateHour } = params
+    const insertResult = await this.dataSource
+      .createQueryBuilder()
+      .insert()
+      .into(PgPayable)
+      .values([
+        { value, status, paymentDateHour }
+      ])
+      .execute()
 
-      return insertResult.identifiers[0].id
-    } catch (error) {
-      return 0
-    }
+    return insertResult.identifiers[0].id
   }
 
   async saveTransaction (params: ProcessTransactionRepo.TrasactionParams): Promise<ProcessTransactionRepo.TransactionResult> {
