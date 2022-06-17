afe24743a21b9781183700bc80a710c1478ab3c1
mathed1as <matheus.vex@gmail.com>
2022-06-17 16:15:10 -0300

    test: refactor

--- tests/data/services/process-transaction.spec.ts
+++ tests/data/services/process-transaction.spec.ts
@@ -8,44 +8,22 @@ describe('ProcessTransactionService', () => {
       paymentMethod: PaymentMethod.credit_card,
       description: 'teste',
       cardNumber: 4854,
-      cardHolderName: 'matheus l d silva',
+      cardHolderName: 'Fulano Teste de Tal',
       cardExpiringDate: '01/22',
       securityCode: 123
     }
 
+    const transactionIdMock = 30
+
     const repoMock = {
       savePayable: jest.fn(),
-      saveTransaction: jest.fn().mockReturnValueOnce(30)
+      saveTransaction: jest.fn().mockReturnValueOnce(transactionIdMock)
     }
 
     const processTransactionService = new ProcessTransactionService(repoMock)
     const result = await processTransactionService.exec(payload)
 
-    expect(result).toEqual({ id: 30 })
-    expect(repoMock.savePayable).toHaveBeenCalledTimes(1)
-    expect(repoMock.saveTransaction).toHaveBeenCalledTimes(1)
-  })
-
-  it('Should be call savePayable and saveTransaction 1 time and return transactio id', async () => {
-    const payload = {
-      value: 100,
-      paymentMethod: PaymentMethod.credit_card,
-      description: 'teste',
-      cardNumber: 4854,
-      cardHolderName: 'matheus l d silva',
-      cardExpiringDate: '01/22',
-      securityCode: 123
-    }
-
-    const repoMock = {
-      savePayable: jest.fn(),
-      saveTransaction: jest.fn().mockReturnValueOnce(30)
-    }
-
-    const processTransactionService = new ProcessTransactionService(repoMock)
-    const result = await processTransactionService.exec(payload)
-
-    expect(result).toEqual({ id: 30 })
+    expect(result).toEqual({ id: transactionIdMock })
     expect(repoMock.savePayable).toHaveBeenCalledTimes(1)
     expect(repoMock.saveTransaction).toHaveBeenCalledTimes(1)
   })
---
+++ tests/infra/typeorm/process-transaction-repo.spec.ts
@@ -0,0 +1,5 @@
+describe('teste', () => {
+  it('', () => {
+    console.log('teste')
+  })
+})
