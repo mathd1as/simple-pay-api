0e35b8ae3f7c18ccf85a74c656c322a0ed494a7f
mathed1as <matheus.vex@gmail.com>
2022-06-17 08:34:14 -0300

    test: test ProcessTransactionService result

--- tests/data/services/process-transaction.spec.ts
+++ tests/data/services/process-transaction.spec.ts
@@ -2,24 +2,50 @@ import { ProcessTransactionService } from '@/data/services'
 import { PaymentMethod } from '@/domain/models'
 
 describe('ProcessTransactionService', () => {
-  it('Should be call savePayable and saveTransaction 1 time', async () => {
+  it('Should be call savePayable and saveTransaction 1 time and return transactio id', async () => {
     const payload = {
       value: 100,
       paymentMethod: PaymentMethod.credit_card,
+      description: 'teste',
       cardNumber: 4854,
       cardHolderName: 'matheus l d silva',
-      validity: new Date(),
+      cardExpiringDate: '01/22',
       securityCode: 123
     }
 
     const repoMock = {
       savePayable: jest.fn(),
-      saveTransaction: jest.fn()
+      saveTransaction: jest.fn().mockReturnValueOnce(30)
     }
 
     const processTransactionService = new ProcessTransactionService(repoMock)
-    await processTransactionService.exec(payload)
+    const result = await processTransactionService.exec(payload)
 
+    expect(result).toEqual({ id: 30 })
+    expect(repoMock.savePayable).toHaveBeenCalledTimes(1)
+    expect(repoMock.saveTransaction).toHaveBeenCalledTimes(1)
+  })
+
+  it('Should be call savePayable and saveTransaction 1 time and return transactio id', async () => {
+    const payload = {
+      value: 100,
+      paymentMethod: PaymentMethod.credit_card,
+      description: 'teste',
+      cardNumber: 4854,
+      cardHolderName: 'matheus l d silva',
+      cardExpiringDate: '01/22',
+      securityCode: 123
+    }
+
+    const repoMock = {
+      savePayable: jest.fn(),
+      saveTransaction: jest.fn().mockReturnValueOnce(30)
+    }
+
+    const processTransactionService = new ProcessTransactionService(repoMock)
+    const result = await processTransactionService.exec(payload)
+
+    expect(result).toEqual({ id: 30 })
     expect(repoMock.savePayable).toHaveBeenCalledTimes(1)
     expect(repoMock.saveTransaction).toHaveBeenCalledTimes(1)
   })
--- tests/index.spec.ts
+++
@@ -1,5 +0,0 @@
-describe('Name of the group', () => {
-  it('should ', () => {
-    console.log('teste')
-  })
-})
