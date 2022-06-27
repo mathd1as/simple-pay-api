b39b0f4411ea0cc9254ac9e03f20e85bff7f63f0
mathed1as <matheus.vex@gmail.com>
2022-06-27 18:54:41 -0300

    refactor express middleware adpater

--- src/infra/express/express-middleware.ts
+++ src/infra/express/express-middleware.ts
@@ -3,6 +3,7 @@ import { RequestHandler } from 'express'
 type Adapter = (middleware: any) => RequestHandler
 
 export const adaptExpressMiddleware: Adapter = middleware => async (req, res, next) => {
+  req.headers.authorization = req.headers.authorization?.split(' ')[1]
   const { statusCode, data } = await middleware.handle({ ...req.headers })
   if (statusCode === 200) {
     req.locals = data
