d7a7b2ed4e7882e2a4c0f891cf2953d2c1a4a1c9
mathed1as <matheus.vex@gmail.com>
2022-07-15 11:41:26 -0300

    modify jwt expiration time

--- src/infra/jwt/jwt-token-handler.ts
+++ src/infra/jwt/jwt-token-handler.ts
@@ -6,8 +6,8 @@ export class JwtTokenHandler implements TokenGenerator, TokenValidator {
   constructor (private readonly secret: string) {}
 
   async generate ({ expirationInMs, key }: TokenGenerator.Input): Promise<TokenGenerator.Output> {
-    const expirationInSeconds = expirationInMs / 1000
-    return sign({ key }, this.secret, { expiresIn: expirationInSeconds })
+    // const expirationInSeconds = expirationInMs / 1000
+    return sign({ key }, this.secret, { expiresIn: 500 })
   }
 
   async validate ({ token }: TokenValidator.Input): Promise<TokenValidator.Output> {
